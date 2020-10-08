27038cee54e69dcd533f87305a3a987ea1030c5f
Omkeshwari Pilare <omkeshwari1997@gmail.com>
2020-10-08 14:55:09 +0530

    Commit: Winning Player or Tie

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -2,25 +2,27 @@
 
 public class TicTacToeGame 
 {
+    public static String user = "", computer ="";
     String board[] = new String[10];
     int boardFlag[] = new int[10];
-    String user = "1", computer ="2";
     String option = "";
 
+    //UC1
     public void createBoard()
     {
         for(int i = 0; i < board.length; i++)
 		{
             board[i] = "";
-            boardFlag[i] = 0;
+            boardFlag[i] = 00;
 		}
     }
 
-    public String chooseOption()
+    //UC2
+    public void chooseOption(String player)
     {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter option X/O:");
+        System.out.println("Enter option user chooses to play X/O:");
         option = scanner.next();
 
         if(option.equals("X") || option.equals("O"))
@@ -41,9 +43,11 @@ public class TicTacToeGame
                 System.out.println("Invalid Input");
         }
 
-        return user;
+        System.out.println("User plays with symbol : " + user);
+        System.out.println("Computer plays with symbol : " + computer);
     }
 
+    //UC3
     void showBoard()
     {
         for(int i = 1; i < 10 ; i++)
@@ -57,25 +61,12 @@ public class TicTacToeGame
         }
     }
 
-    int makeMove(String input)
+    //UC4 UC5
+    void makeMove(String input)
     {
         Scanner scanner = new Scanner(System.in);
-        int index = 0, flag = 0, flagFull = 1;
-
-        for(int i = 0; i < boardFlag.length; i++)
-        {
-            if(boardFlag[i] == 0)
-            {
-                flagFull = 0;
-            }
-        }
-
-        if(flagFull == 1)
-        {
-            return 1;
-        }
-        else
-        {
+        int index = 0, flag = 0;
+       
             do
             {
                 flag = 0;
@@ -93,7 +84,7 @@ public class TicTacToeGame
                     if(board[index-1].equals(""))
                     {
                         board[index-1] = input;
-                        boardFlag[index-1] = 1;
+                        boardFlag[index-1] = 1 ;
                     }
                     else
                     {
@@ -103,15 +94,149 @@ public class TicTacToeGame
                 }
 
             }while(flag == 1);        
-        }
-
-        return 0;
     }
 
+    //UC6
     int choosingToss()
     {
         int options=1 + (int)(Math.random() * ((2 - 1) + 1));
-        return int;
+        return options;
+    }
+
+    //UC7
+    int gameResult()
+    {
+        int flagresult = 0;
+        int flag = 0;
+
+        
+            //if((board[0]+board[4]+board[8]).equals("XXX") || (board[0]+board[4]+board[8]).equals("OOO"))
+            if(board[0].equals(board[4]) && board[0].equals(board[8]) && (board[0].equals("X") || board[0].equals("O")))
+            {
+                flagresult = 1;
+                return flagresult;
+            }
+            //elseif((board[2]+board[4]+board[6]).equals("XXX") || (board[2]+board[4]+board[6]).equals("OOO"))
+            else if(board[2].equals(board[4]) && board[2].equals(board[6]) && (board[2].equals("X") || board[2].equals("O")))
+            {
+                flagresult = 1;
+                return flagresult;
+            }
+            //if((board[0]+board[1]+board[2]).equals("XXX") || (board[0]+board[1]+board[2]).equals("OOO"))
+            else if(board[0].equals(board[1]) && board[0].equals(board[2]) && (board[0].equals("X") || board[0].equals("O")))
+            {
+                flagresult = 1;
+                return flagresult;
+            }
+            //if((board[3]+board[4]+board[5]).equals("XXX") || (board[3]+board[4]+board[5]).equals("OOO"))
+            else if(board[3].equals(board[4]) && board[3].equals(board[5]) && (board[3].equals("X") || board[3].equals("O")))
+            {
+                flagresult = 1;
+                return flagresult;
+            }
+            //elseif((board[6]+board[7]+board[8]).equals("XXX") || (board[6]+board[7]+board[8]).equals("OOO"))
+            else if(board[6].equals(board[7]) && board[6].equals(board[8]) && (board[6].equals("X") || board[6].equals("O")))
+            {
+                flagresult = 1;
+                return flagresult;
+            }
+            //elseif((board[0]+board[3]+board[6]).equals("XXX") || (board[0]+board[3]+board[6]).equals("OOO"))
+            else if(board[0].equals(board[3]) && board[0].equals(board[6]) && (board[0].equals("X") || board[0].equals("O")))
+            {
+                flagresult = 1;
+                return flagresult;
+            }
+            //if((board[1]+board[4]+board[7]).equals("XXX") || (board[1]+board[4]+board[7]).equals("OOO"))
+            else if(board[1].equals(board[4]) && board[1].equals(board[7]) && (board[1].equals("X") || board[1].equals("O")))
+            {
+                flagresult = 1;
+                return flagresult;
+            }
+            //elseif((board[2]+board[5]+board[8]).equals("XXX") || (board[2]+board[4]+board[8]).equals("OOO"))
+            else if(board[2].equals(board[5]) && board[2].equals(board[8]) && (board[2].equals("X") || board[2].equals("O")))
+            {
+                flagresult = 1;
+                return flagresult;
+            } 
+            else
+            {
+                for(int i = 0; i < boardFlag.length ; i++)
+                {
+                    flagresult =0;
+
+                    if(boardFlag[i] == 1)
+                    {
+                        flagresult = 2;
+                    }
+                }
+            }
+
+        return flagresult;
+    }
+
+    void game(String player)
+    {
+        TicTacToeGame gameObject = new TicTacToeGame();
+        int gameresult, count = 0;
+        String currentPlayer = "";
+
+        gameObject.createBoard();
+        gameObject.chooseOption(player);
+
+        do
+        {   
+            if(player == "User")
+            {
+                if(count % 2 == 0)
+                {
+                    currentPlayer = "User";
+                }
+                else
+                {
+                    currentPlayer = "Computer";
+                }
+                count++;
+            }
+
+            if(player == "Computer")
+            {
+                if(count % 2 == 0)
+                {
+                    currentPlayer = "Computer";
+                }
+                else
+                {
+                    currentPlayer = "User";
+                }
+                count++;
+            }
+
+            System.out.println(currentPlayer + " playing");
+            //System.out.println(user+"----"+computer);
+            if(currentPlayer == "User")
+            {
+                gameObject.makeMove(user);
+            }
+            
+            if(currentPlayer == "Computer")
+            {
+                gameObject.makeMove(computer);
+            }
+
+            gameObject.showBoard();
+            gameresult = gameObject.gameResult();
+            
+            System.out.println("RESULT:"+gameresult);
+            if(gameresult == 1)
+            {
+                System.out.println(currentPlayer+" wins");
+            }
+            if(gameresult == 2)
+            {
+                System.out.println("Tie match");
+            }
+
+        }while(gameresult!= 1 && gameresult!=2);
     }
 
 	public static void main(String[] args) 
@@ -124,13 +249,17 @@ public class TicTacToeGame
         int toss = gameObject.choosingToss();
         if(toss == 1)
         {
-            System.out.println("User first");
+            System.out.println("User starts Game");
+            gameObject.game("User");
         }
         if(toss ==2 )
         {
-            System.out.println("Computer First");
+            System.out.println("Computer starts Game");
+            gameObject.game("Computer");
         }
 
+        //gameObject.gameresult("User");
+        /*
         do
         {
             System.out.println("1. show board");
@@ -150,7 +279,7 @@ public class TicTacToeGame
                         }
                         break;
             }
-        }while(choice>=1 && choice<=4);
+        }while(choice>=1 && choice<=4);*/
 	}
 
 }
